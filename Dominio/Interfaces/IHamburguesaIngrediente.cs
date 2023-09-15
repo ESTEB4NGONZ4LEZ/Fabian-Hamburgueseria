cd29358398bc98f1889080b2fd84e4b3200074d5
ESTEB4NGONZ4LEZ <fabestebece@gmail.com>
2023-09-15 07:32:22 -0500

    :sparkles: feat: Creacion de las interfaces

---
+++ Dominio/Interfaces/IHamburguesaIngrediente.cs
@@ -0,0 +1,18 @@
+
+using System.Linq.Expressions;
+using Dominio.Entities;
+
+namespace Dominio.Interfaces;
+
+public interface IHamburguesaIngrediente
+{
+    Task<IEnumerable<HamburguesaIngrediente>> GetAllAsync();
+    Task<HamburguesaIngrediente> GetByIdAsync(int hamburguesaId, int ingredienteId);
+    void Add(HamburguesaIngrediente entity);
+    void AddRange(IEnumerable<HamburguesaIngrediente> entities);
+    void Update(HamburguesaIngrediente entity);
+    void Remove(HamburguesaIngrediente entity);
+    void RemoveRange(IEnumerable<HamburguesaIngrediente> entities);
+    IEnumerable<HamburguesaIngrediente> Find(Expression<Func<HamburguesaIngrediente, bool>> expression);
+    Task<(int totalRegistros, IEnumerable<HamburguesaIngrediente> registros)> GetAllAsync(int pageIndex, int pageSize, string search);
+}
