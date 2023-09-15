6288af89b4957bf254f004d19838f32df763ab37
ESTEB4NGONZ4LEZ <fabestebece@gmail.com>
2023-09-15 07:34:40 -0500

    :sparkles: feat: Configuracion de la Interfas de la unidad de trabajo

--- Dominio/Interfaces/IUnitOfWork.cs
+++ Dominio/Interfaces/IUnitOfWork.cs
@@ -2,10 +2,15 @@
 
 namespace Dominio.Interfaces;
 
-    public interface IUnitOfWork
-    {
-         IUsuario Usuarios {get;}
-         IRol Roles {get;}
-        Task<int> SaveAsync();
-    }
+public interface IUnitOfWork
+{
+    ICategoria Categoria { get; }
+    IChef Chef { get; }
+    IHamburguesa Hamburguesa { get; }
+    IHamburguesaIngrediente HamburguesaIngrediente { get; }
+    IIngrediente Ingrediente { get;}
+    IUsuario Usuarios {get;}
+    IRol Roles {get;}
+    Task<int> SaveAsync();
+}
 
